487b045e3e581f74074697ef4ab08a61eff99d2f
nico cheppi <nicocheppi@hotmail.com>
2025-04-13 00:32:33 -0300

    base

---
+++ prueba.py
@@ -0,0 +1,10 @@
+
+radio = input("Ingrese el radio")
+radio = float(radio)
+
+pi = 3.14
+
+area = pi * (radio ** 2)
+perimetro = 2 * pi * radio
+
+print("El area del circulo es " , area , " y el perimetro es " , perimetro , ".")
