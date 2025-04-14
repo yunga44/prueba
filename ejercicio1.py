c1dae6cd08f5c3a11b4034d5900a13a9354a6fa3
nico cheppi <nicocheppi@hotmail.com>
2025-04-14 17:35:01 -0300

    se modifico ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,6 @@
 
-nombre = "Nico"
-apellido = "cheppi"
+nombre = input("Ingrese su nombre")
+apellido = input("Ingrese su apellido")
 materia = "programacion 1"
 
 print(f"hola soy {nombre} {apellido} estudiante de {materia}")
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,5 @@
 nombre = input("Ingrese su nombre")
 apellido = input("Ingrese su apellido")
-materia = "programacion1"
+materia = "programacion 1"
 
 print(f"hola soy {nombre} {apellido} estudiante de {materia}")
